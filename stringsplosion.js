8c6dd0d75e2c4bde0ecbf036ecd02d28319c1abf
ludralph <raphaelumoh@gmail.com>
2017-05-11 15:24:45 +0100

    improved version of stringsplosion

--- stringsplosion.js
+++ stringsplosion.js
@@ -5,20 +5,13 @@ exports.StringSplosion = class StringSplosion {
     this.word = word;
   }
    manipulate(word){
-    let newWordLength = (this.word.length * (this.word.length +1)) / 2;
+    
     let validWord = "";
-    let newWord = new Array(newWordLength)
-    let index = 0;
-    for(let i = 0; i < this.word.length; i++){
-      for (let j = 0; j <= i; j++){
-        newWord[index] = this.word.charAt(j);
-        index++;
-      }
+    for (let i =0; i < this.word.length; i++){
+      validWord = validWord + this.word.substring(0,i+1);
     }
-    for(let i = 0; i < newWord.length; i++){
-      validWord = validWord + newWord[i];
-    }
-    return validWord;
+    return  validWord;
+
 
   }
 
